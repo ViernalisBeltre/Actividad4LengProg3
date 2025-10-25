bced7c242676c1a0faaf7b11fc4f5602d00570ad
Ezequiel Santos <214915526+ViernalisBeltre@users.noreply.github.com>
2025-10-25 19:36:32 -0400

    Modelo Carreras

---
+++ Actividad4LengProg3/Models/CarreraViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class CarreraViewModel
+    {
+        [Required(ErrorMessage = "Digite el código de la carrera")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Seleccione la carrera")]
+        [Display(Name = "Nombre de la Carrera")]
+        public string NombreCarrera { get; set; }
+
+        [Required(ErrorMessage = "Digite la cantidad de créditos")]
+        [Display(Name = "Cantidad de Créditos")]
+        public int CantidadCreditos { get; set; }
+
+        [Required(ErrorMessage = "Digite la cantidad de materias")]
+        [Display(Name = "Cantidad de Materias")]
+        public int CantidadMaterias { get; set; }
+    }
+}
