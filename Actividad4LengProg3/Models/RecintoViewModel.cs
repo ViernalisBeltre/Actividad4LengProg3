1efbc8754c731209c83e942eae6bf4413392686f
Ezequiel Santos <214915526+ViernalisBeltre@users.noreply.github.com>
2025-10-25 19:33:57 -0400

    Modelo Recinto

---
+++ Actividad4LengProg3/Models/RecintoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad4LengProg3.Models
+{
+    public class RecintoViewModel
+    {
+        [Required(ErrorMessage = "Digite el código del recinto")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Digite el nombre del recinto")]
+        [StringLength(100)]
+        [Display(Name = "Nombre del Recinto")]
+        public string NombreRecinto { get; set; }
+
+        [Required(ErrorMessage = "Digite la dirección del recinto")]
+        [StringLength(100)]
+        [Display(Name = "Dirección del Recinto")]
+        public string DireccionRecinto { get; set; }
+    }
+}
